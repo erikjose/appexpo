911a4ea43c22b062def4e7ff787ce8718cb9b1a4
Erik <erik@wizzer.com.br>
2020-01-06 12:07:52 -0300

    Config routes

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import Router from "./src/router";
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Obeso Infeccionado</Text>
-    </View>
-  );
+  return <Router />;
 }
 
 const styles = StyleSheet.create({
